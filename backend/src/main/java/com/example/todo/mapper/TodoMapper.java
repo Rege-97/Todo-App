e8553398652cba8e20403be6f99c693062729153
Rege-97 <cogus1027@gmail.com>
2025-08-09 14:39:44 +0900

    :sparkles: feat: TodoMapper를 추가하여 할 일 CRUD 기능 구현

---
+++ backend/src/main/java/com/example/todo/mapper/TodoMapper.java
@@ -0,0 +1,22 @@
+package com.example.todo.mapper;
+
+import com.example.todo.domain.Todos;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface TodoMapper {
+
+    void insert(Todos todos);
+
+    List<Todos> findByUserId(@Param("userId") Long userId);
+
+    Todos findByIdAndUserId(@Param("id") Long id, @Param("userId") Long userId);
+
+    void update(Todos todos);
+
+    void deleteByIdAndUserId(@Param("id") Long id, @Param("userId") Long userId);
+
+}
