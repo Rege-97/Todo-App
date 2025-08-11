6641b3ccc6bc50d4f763bd6b6e6547b6efd41a49
Rege-97 <cogus1027@gmail.com>
2025-08-11 22:11:36 +0900

    ✨ feat: 앱 시작 시 토큰을 확인하여 자동 로그인 기능 구현

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./src/screens/LoginScreen";
 import MainScreen from "./src/screens/MainScreen";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { ActivityIndicator, Text, View } from "react-native";
 
 export type RootStackParamList = {
   Login: undefined;
@@ -12,15 +14,47 @@ export type RootStackParamList = {
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [userToken, setUserToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const checkToken = async () => {
+      try {
+        const token = await AsyncStorage.getItem("accessTokken");
+        setUserToken(token);
+      } catch (error) {
+        console.log("토큰 확인 중 에러 발생", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    checkToken();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+        <Text>앱을 로딩하고 있습니다...</Text>
+      </View>
+    );
+  }
+
   return (
-  
     <NavigationContainer>
       {/**브라우저 라우터 처럼 네비게이션 상태를 관리하고 감싸주는 최상위 컴포넌트 */}
       <Stack.Navigator initialRouteName="Login">
         {/**여러 화면들을 스택 방식으로 관리하는 컨테이너 */}
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Main" component={MainScreen} />
-        {/**개별 화면을 등록하는 컴포넌트 */}
+        {userToken ? (
+          <Stack.Screen name="Main" component={MainScreen} />
+        ) : (
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
