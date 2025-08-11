afe551b5ea97aef9eb2f825b32586f1a01e4f2b4
Rege-97 <cogus1027@gmail.com>
2025-08-11 20:39:12 +0900

    ✨ feat: React Navigation 설정 및 로그인 화면 추가

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./src/screens/LoginScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer> {/**브라우저 라우터 처럼 네비게이션 상태를 관리하고 감싸주는 최상위 컴포넌트 */}
+      <Stack.Navigator>  {/**여러 화면들을 스택 방식으로 관리하는 컨테이너 */}
+        <Stack.Screen name="Login" component={LoginScreen} /> {/**개별 화면을 등록하는 컴포넌트 */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
